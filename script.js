1e8fe70788a25d7c1db1aa29e232a9023d9e550f
anthony75943 <anthony.vaz@escola.pr.gov.br>
2025-10-17 08:01:37 -0300

    algoritimos

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const avanca = document.querySelectorAll;('.btn-proximo');
 avanca. forEach(button => {
     button.addEventListener('click', function(){
        const atual = document.queryselector('.ativo');
-       const proximoPasso = 'passso-'+ this.getAttribute('data-proximo')
+       const proximoPasso = 'passso-'+ this.getAttribute('data-proximo');
+
+       atual.
     })  
 })
